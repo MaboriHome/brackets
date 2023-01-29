7db66122a19d6936f69149cf708f37d1f8df2e93
Алексей Макеев <117520389+MaboriHome@users.noreply.github.com>
2023-01-29 12:25:48 +0300

    init: create func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false
+  }
+
+  let brackets = '';
+  let tempStr = str;
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      brackets = bracketsConfig[j][0] + bracketsConfig[j][1];
+      tempStr = tempStr.replace(brackets, '');
+    }
+  }
+
+  return tempStr.length === 0;
+};
